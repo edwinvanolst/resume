d3b7483572bd72d6d1883a1d84c5bb76f91be32d
Edwin van Olst <edwinvanolst@me.com>
2019-01-23 05:16:23 +0100

    cleanup

--- old/public/js/message.js
+++
@@ -1,7 +0,0 @@
-var messageButton = document.getElementById('message-button')
-var messageForm = document.getElementById('message-form')
-
-messageButton.onclick = function() {
-  messageForm.style.display = 'block'
-}
-
